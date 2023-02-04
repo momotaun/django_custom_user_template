5e7dce2f210a2b846ba26cd3b8cda8ba641cf638
momotaun <momotaun@student.wethinkcode.co.za>
2023-02-04 13:20:36 +0200

    added static and media settings

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from dj_rest_auth.registration.views import VerifyEmailView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,9 @@ urlpatterns = [
     # Custom Urls
     path('api/auth/', include('users.api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
